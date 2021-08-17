9baa533a99c2a2847832059bd5d40aafa14d0f46
davinci360 <jasonbrowntime@gmail.com>
2021-08-17 05:45:50 -0400

    3 passing

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*`customerName`: Declare a variable in global scope called `customerName` using
   the `var` keyword.*/
 var customerName = 'bob';
+var bestCustomer;
 function returnCustomerName() {
 return customerName;
 }
@@ -8,4 +9,8 @@ function upperCaseCustomerName() {
         customerName = customerName.toUpperCase();
    /* 'modifies the customerName variable'*/
 
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
 }
